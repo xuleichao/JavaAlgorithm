6cbcc30847f537d85e1744cc51dac7bc24142a5e
xuleichao <1205448156@qq.com>
2018-12-06 10:02:19 +0800

    实现List 的增删

--- JavaMethod/src/demo.java
+++ JavaMethod/src/demo.java
@@ -1,19 +1,29 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+
+/*
+ 1. asList 可以将数组转化为list，但是这种list 不能增删里面的元素，否则报错
+ 2. 改用Colletions 将数组转化为list，也有其他的方式。
+ */
 
 public class demo {
 	public static void main(String[] args){
 		String[] a={"0","1","2","3","4","5"};
-		//String[] str=a.toString();
-		List<String> a_list = Arrays.asList(a);
-		int z = a_list.size();
+		List<String> a_list = new ArrayList<String>(a.length);
+		Collections.addAll(a_list, a);
+		
 		System.out.println(a_list);
-		System.out.println(z);
 		int x=5;
-		a_list.remove(x);
+		a_list.remove(4);
+		System.out.println(a_list);
+		int z = a_list.size();
 		for(int i=0;i<z;i++){
 			System.out.println(a_list.get(i));
 		}
+		a_list.add(0, "9");
+		System.out.println(a_list);
 	}
 
 }
