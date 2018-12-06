8474b17d62f596250744e90e629ea1e0c7b9d8c4
xuleichao <1205448156@qq.com>
2018-12-06 08:11:09 +0800

    哪里有变化

--- JavaMethod/src/MergeSorted.java
+++ JavaMethod/src/MergeSorted.java
@@ -1,44 +1,40 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class MergeSorted {
 	public static void main(String[] args){
 		double[] a = {1,2,3};
-		SortedMain(a);
+		double[] b = {2,4,3};
+		merge(a, b);
 	}
 	public static double[] SortedMain(double[] input){
-		int len = input.length;
-		double[] NumArray = null;
-		if(len == 1){
-			NumArray = input;
-		}
-		else if(len == 2){
-			double i = input[0];
-			double j = input[1];
-			if(i >= j){
-				NumArray[0] = i;
-				NumArray[1] = j;
+		return input;
+		
+	}
+	
+	public static double[] merge(double[] left, double[] right){
+		List<double[]> leftArray = Arrays.asList(left);
+		List<double[]> rightArray = Arrays.asList(right);
+		List<double[]> newArray = new ArrayList<>();
+		int left_len = left.length;
+		int right_len = right.length;
+		//进行数组首位比较合并
+		while (left_len!=0 & right_len!=0){
+			if (left[0]>=right[0]){
+				double[] z = rightArray.get(0);
+				newArray.add(z);
+				rightArray.remove(0);
 			}
 			else{
-				NumArray[0] = j;
-				NumArray[1] = i;
+				double[] z = leftArray.get(0);
+				newArray.add(z);
+				leftArray.remove(0);
 			}
 		}
-		else{
-			int split_0 = len / 2;
-			int split_1 = len - split_0;
-			System.out.println(split_1);
-			int i,j;
-			double[] input_0 = null;
-			double[] input_1 = null;
-			for(i=0;i<split_0;i++){
-				input_0[i] = input[i];
-			}
-			for(j=split_0;j<split_1;j++){
-				input_1[j] = input[j];
-			}
-			System.out.println(Arrays.toString(input_0));
-			System.out.println(Arrays.toString(input_1));
-		}
-		return NumArray;
+		//System.out.printf(newArray,leftArray,rightArray);
+		
+		return right;
+		
 	}
 }
