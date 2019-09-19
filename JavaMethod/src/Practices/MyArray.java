fa3639c0ac910c238601a7cfd70e0e09a1cb2132
xuleichao <1205448156@qq.com>
2019-09-19 11:59:45 +0800

    dd

--- JavaMethod/src/Practices/MyArray.java
+++ JavaMethod/src/Practices/MyArray.java
@@ -1,5 +1,25 @@
 package Practices;
 
 public class MyArray {
+	private int[] array;
+	private int size;
+	
+	// 构造方法
+	public MyArray(int capacity){
+		array = new int[capacity];
+		size = 0;
+	}
+	
+	// 默认参数构造方法
+	public MyArray(){
+		this(10);
+	}
+	
+	public void add(int index, int e){
+		if (index < 0 | index > size){
+			throw new IllegalArgumentException("索引超出范围");
+		}
+	}
+	
 
 }
