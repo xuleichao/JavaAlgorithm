2b1a4c745d3ed53c7378d506cbd40f81dfbe7118
xuleichao <1205448156@qq.com>
2019-09-21 11:03:50 +0800

    泛型

--- JavaMethod/src/Practices/MyArray.java
+++ JavaMethod/src/Practices/MyArray.java
@@ -1,12 +1,12 @@
 package Practices;
 
-public class MyArray {
-	private int[] array;
+public class MyArray<E> {
+	private E[] array;
 	private int size;
 	
 	// 构造方法
 	public MyArray(int capacity){
-		array = new int[capacity];
+		array = (E[]) new Object [capacity];
 		size = 0;
 	}
 	
@@ -15,7 +15,7 @@ public class MyArray {
 		this(10);
 	}
 	
-	public void add(int index, int e){
+	public void add(int index, E e){
 		if (index < 0 | index > size){
 			throw new IllegalArgumentException("索引超出范围");
 		}
@@ -46,16 +46,16 @@ public class MyArray {
 		StringBuilder res = new StringBuilder();
 		res.append(String.format("array is size = %d, capacity = %d\n", size, array.length));
 		for(int i = 0;i < size;i++){
-			res.append(String.format("the %d element is %d\n", i, array[i]));
+			res.append(String.format("the %d element is %s\n", i, array[i].toString()));
 		}
 		return res.toString();
 		
 	}
 	
 	//查找元素并返回对应的索引
-	public int find(int ele){
+	public int find(E ele){
 		for(int i = 0; i < size; i++){
-			if (array[i] == ele){
+			if (array[i].equals(ele)){
 				return i;
 			}
 		}
@@ -63,9 +63,9 @@ public class MyArray {
 	}
 	
 	//是否包含元素
-	public boolean contains(int ele){
+	public boolean contains(E ele){
 		for(int i = 0; i < size; i++){
-			if (array[i] == ele){
+			if (array[i].equals(ele)){
 				return true;
 			}
 		}
@@ -73,7 +73,7 @@ public class MyArray {
 	}
 	
 	//根据索引 get 元素
-	public int get(int idx){
+	public E get(int idx){
 		return array[idx];
 	}
 	
@@ -81,8 +81,25 @@ public class MyArray {
 		return size == 0;
 	}
 	
+	
+	//remove by index
+	public void remove(int idx){
+		if (idx < 0 | idx > size){
+			throw new IllegalArgumentException("索引超出范围");
+		}
+		
+		if (size == 0){
+			throw new IllegalArgumentException("试图从空数组中删除数据");
+		}
+		
+		for(int i = idx + 1; i < size; i++){
+			array[i - 1] = array[i];
+		}
+		size = size - 1;
+	}
+	
 	public static void main(String[] args){
-		MyArray arr = new MyArray(20);
+		MyArray <Object> arr = new MyArray<>(20);
 		for(int i = 0; i < 10; i++)
 			arr.add(i, i);
 		System.out.println(arr);
@@ -91,6 +108,9 @@ public class MyArray {
 		System.out.println(arr.contains(-2));
 		System.out.println(arr.contains(-1));
 		System.out.println(arr.find(4));
+		arr.remove(11);
+		arr.add(6, "ttt");
+		System.out.println(arr);
 		
 	}
 }
