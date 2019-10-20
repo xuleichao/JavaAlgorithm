96134de10b670e59a403f668faa589528dea569d
xuleichao <1205448156@qq.com>
2019-10-20 22:29:48 +0800

    bst

---
+++ JavaMethod/src/Practices/BST.java
@@ -0,0 +1,17 @@
+package Practices;
+
+public class BST {
+	
+	private class Node{
+		int val;
+		Node left;
+		Node right;
+		
+		public Node(int ele){
+			val = ele;
+			left = null;
+			right = null;
+		}
+	}
+
+}
